dcecc54f686042481929c5dcf5ffe0593914b2e7
tyagi-4080 <tyagi.anurag2004@gmail.com>
2025-09-30 16:52:19 +0530

    commit the files

---
+++ PGLIFE/includes/footer.php
@@ -0,0 +1,28 @@
+<?php
+  //PHP variable storing property list page path
+  
+  //$property_list_path = "/PGLIFE/property_list.php"; this line creates an error 
+
+  $property_list_path = "property_list.php"; // i replace this line with this one
+?>
+
+<!-- Footer Section -->
+<div class="footer">
+    <div class="page-container footer-container">
+        <div class="footer-cities">
+            <div class="footer-city">
+                <a href="<?php echo $property_list_path.'?city=Delhi'; ?>">PG in Delhi</a>
+            </div>
+            <div class="footer-city">
+                <a href="<?php echo $property_list_path.'?city=Mumbai'; ?>">PG in Mumbai</a>
+            </div>
+            <div class="footer-city">
+                <a href="<?php echo $property_list_path.'?city=Bangalore'; ?>">PG in Bangalore</a>
+            </div>
+            <div class="footer-city">
+                <a href="<?php echo $property_list_path.'?city=Hyderabad'; ?>">PG in Hyderabad</a>
+            </div>
+        </div>
+        <div class="footer-copyright">© 2025 Copyright PG Life </div>
+    </div>
+</div>
