9c75a10f10e2b02490ebc964004680775d421cbf
tyagi-4080 <tyagi.anurag2004@gmail.com>
2026-05-18 01:20:50 +0530

    Commit changes

--- PGLIFE/includes/database_connect.php
+++ PGLIFE/includes/database_connect.php
@@ -1,8 +1,22 @@
-<?php
+<!-- <?php
   $db_hostname = "127.0.0.1";
   $db_username = "root";
-  $db_password = "";
+  $db_password = "root4080";
   $db_name = "pg_life";
 
   $con = mysqli_connect( $db_hostname , $db_username , $db_password , $db_name );
+?> -->
+
+
+<?php
+$conn = mysqli_connect(
+    "127.0.0.1",
+    "root",
+    "root4080",
+    "pg_life"
+);
+
+if (!$conn) {
+    die("Connection failed: " . mysqli_connect_error());
+}
 ?>
--- PGLIFE/includes/database_connect_hide_error.php
+++ PGLIFE/includes/database_connect_hide_error.php
@@ -1,10 +1,25 @@
-<?php
+<!-- <?php
   $db_hostname = "127.0.0.1";
   $db_username = "root";
-  $db_password = "";
+  $db_password = "root4080";
   $db_name = "pg_life";
 
   // This function will not echo/show any error on the webpage even if the function fails or there is some error...
   // Using it so the result of this page will only be the JSON encoded string to be parsed by the AJAX requests...
   $con = @mysqli_connect( $db_hostname , $db_username , $db_password , $db_name );
+?> -->
+
+
+
+<?php
+$conn = mysqli_connect(
+    "127.0.0.1",
+    "root",
+    "root4080",
+    "pg_life"
+);
+
+if (!$conn) {
+    die("Connection failed: " . mysqli_connect_error());
+}
 ?>
