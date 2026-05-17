7c66aa568944f83379be61e0d501967addeb23f5
tyagi-4080 <tyagi.anurag2004@gmail.com>
2026-05-18 01:23:14 +0530

    Update footer.php

--- PGLIFE/includes/footer.php
+++ PGLIFE/includes/footer.php
@@ -23,6 +23,6 @@
                 <a href="<?php echo $property_list_path.'?city=Hyderabad'; ?>">PG in Hyderabad</a>
             </div>
         </div>
-        <div class="footer-copyright">© 2025 Copyright PG Life </div>
+        <div class="footer-copyright">© 2026 Copyright PG Life </div>
     </div>
 </div>
